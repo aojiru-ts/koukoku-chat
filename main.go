12d45ea7b4ae0a12dbcaf843fbdebea4d8a9031c
aojiru-ts <45072106+aojiru-ts@users.noreply.github.com>
2023-09-06 22:42:11 +0900

    Update main.go

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"crypto/tls"
 	"fmt"
 	"log"
-	"os"
 	"strings"
 	"sync"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
+
+var allText []string
 
 func main() {
 	config := tls.Config{Certificates: []tls.Certificate{}, InsecureSkipVerify: false}
@@ -18,6 +22,31 @@ func main() {
 	}
 	defer conn.Close()
 	log.Println("client: connected to: ", conn.RemoteAddr())
+
+	app := tview.NewApplication()
+	textView := tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetChangedFunc(func() {
+			app.Draw()
+		})
+
+	inputField := tview.NewInputField().
+		SetLabel("Send: ").
+		SetFieldWidth(0)
+
+	inputField.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			text := inputField.GetText()
+			fmt.Fprintln(conn, text)
+			inputField.SetText("")
+		}
+	})
+
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(textView, 0, 1, false).
+		AddItem(inputField, 1, 1, true)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -29,16 +58,16 @@ func main() {
 			if !strings.HasPrefix(line, ">>") {
 				continue
 			}
-			fmt.Println(line)
+			allText = append([]string{line}, allText...)
+			textView.SetText(strings.Join(allText, "\n"))
 		}
 	}()
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			fmt.Fprintln(conn, scanner.Text())
+		if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
+			log.Fatalf("Error running the app: %s", err)
 		}
 	}()
 
